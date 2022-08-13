8f79649f0faf4d474c9caab404355701d257df66
Ritesh Ghosh <ghoshritesh55@gmail.com>
2022-08-13 18:17:15 +0530

    frontend: designing signin page

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -12,3 +12,40 @@ window.addEventListener('load', e => {
 });
 
 
+// const $ = a => document.querySelector(a);
+// const $$ = bb => document.querySelectorAll(bb);
+
+
+
+$('.signin__form__email').addEventListener('focus', e => {
+  e.target.parentElement.classList.add('accent');
+});
+
+$('.signin__form__email').addEventListener('blur', e => {
+  e.target.parentElement.classList.remove('accent');
+});
+
+
+$('.signin__form__password').addEventListener('focus', e => {
+  e.target.parentElement.classList.add('accent');
+});
+
+$('.signin__form__password').addEventListener('blur', e => {
+  e.target.parentElement.classList.remove('accent');
+});
+
+
+$('.signin__form__password').addEventListener('input', e => {
+
+  if(e.target.value.length > 0)
+    $('.signin__form__show-password').classList.add('visible');
+  else
+    $('.signin__form__show-password').classList.remove('visible');
+})
+
+$('.signin__form__show-password').addEventListener('click', e => {
+  e.target.previousElementSibling.type = (e.target.previousElementSibling.type === "password") ? "text" : "password";
+  e.target.textContent = (e.target.textContent === "hide") ? "show" : "hide";
+
+  e.target.previousElementSibling.focus();
+})
