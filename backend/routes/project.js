cf08c5f7d21a2fc07fa09d14505a8babc13f366c
Ritesh Ghosh <ghoshritesh55@gmail.com>
2022-12-15 21:41:21 +0530

    testing project routes

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const projectController = require('../controllers/projectController');
 
+router.route('/')
+  .get(projectController.getProject)
+
 router.route('/:projectId/:section')
   .get(projectController.getProject);
 
