884bd50d343bb9d9a9118933e0f406f81adcfa28
Ritesh Ghosh <ghoshritesh55@gmail.com>
2022-12-15 21:40:35 +0530

    promise wrapped jwt's verify method

--- backend/middlewares/userAuth.js
+++ backend/middlewares/userAuth.js
@@ -1,6 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-const userAuth = (req, res, next) => {
+function jwtVerify(token, secretKey) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      secretKey,
+      (err, decoded) => {
+        if(err) { reject(err); return; }
+        resolve(decoded);
+      }
+    )
+  })
+}
+
+const userAuth = async (req, res, next) => {
   const rToken = req.cookies?.refresh_token;
   if(!rToken) return res.redirect('/signin');
   
@@ -9,26 +22,35 @@ const userAuth = (req, res, next) => {
   const aToken = acCookie && acCookie.split(" ")[1];
   if(aToken == null) return res.redirect(`/refresh?pth=${req.originalUrl}`);
 
+  try {
+    await jwtVerify(rToken, process.env.REFRESH_TOKEN_SECRET);
+    const decoded = await jwtVerify(aToken, process.env.ACCESS_TOKEN_SECRET);
+    req.uuid = decoded.uuid;
+    next();
 
-  jwt.verify(
-    rToken,
-    process.env.REFRESH_TOKEN_SECRET,
-    (err, decoded) => {
-      if (err) return res.redirect('/signin'); 
+  } catch (error) {
+    res.redirect('/signin');
+  }
+
+  // jwt.verify(
+  //   rToken,
+  //   process.env.REFRESH_TOKEN_SECRET,
+  //   (err, decoded) => {
+  //     if (err) return res.redirect('/signin'); 
 
 
-      jwt.verify(
-        aToken,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, decoded) => {
-          if(err) return res.redirect('/signin');
+  //     jwt.verify(
+  //       aToken,
+  //       process.env.ACCESS_TOKEN_SECRET,
+  //       (err, decoded) => {
+  //         if(err) return res.redirect('/signin');
     
-          req.uuid = decoded.uuid;
-          next();
-        }
-      )
-    }
-  )  
+  //         req.uuid = decoded.uuid;
+  //         next();
+  //       }
+  //     )
+  //   }
+  // )  
 }
 
 module.exports = userAuth;
