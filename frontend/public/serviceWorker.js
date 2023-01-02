56b2d2bd3d83f0ef7b9591811476b6488de952b7
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-01-03 03:47:17 +0530

    modified cross-origin manifest file, updated internal css

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,52 +1,49 @@
 const CACHE_NAME = "ProjectFy";
-const assets = [
-  '/',
-  '/fonts/roboto-v30-latin-500.woff', '/fonts/roboto-v30-latin-500.woff2',
-  '/fonts/roboto-v30-latin-regular.woff', '/fonts/roboto-v30-latin-regular.woff2',
-  '/fonts/ubuntu-v20-latin-500.woff', '/fonts/ubuntu-v20-latin-500.woff2',
-  '/fonts/ubuntu-v20-latin-regular.woff', '/fonts/ubuntu-v20-latin-regular.woff2',
+// const assets = [
+//   '/',
+//   '/images/favicon-192x192.png', '/images/favicon-512x512.png', '/images/favicon-512x512.svg',
+//   '/css/homeStyles.css', '/css/rootHomeStyles.css',
+//   '/js/home.overview.js', '/js/home.ideas.js', '/js/home.kanban.js',
+//   '/js/home.root.js', '/js/signinPage.js', '/js/signupPage.js', '/js/utility.js'
+// ];
 
-  '/images/favicon-192x192.png', '/images/favicon-512x512.png', '/images/favicon-512x512.svg',
-
-  '/css/homeStyles.css', '/css/rootHomeStyles.css',
-
-  '/js/home.overview.js', '/js/home.ideas.js', '/js/home.kanban.js',
-  '/js/home.root.js', '/js/signinPage.js', '/js/signupPage.js', '/js/utility.js'
-];
+const assets = [];
 
 // Install
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
+      // return cache.addAll(assets);
     })
   );
 });
 
 // Listen for requests
 self.addEventListener('fetch', (e) => {
-  // e.respondWith(
-  //   fetch(e.request).then(response => response)
-  // );
-
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+    fetch(e.request).then(response => {
+      return response;
     })
   );
+
+  // e.respondWith(
+  //   caches.match(e.request).then(response => {
+  //     return response || fetch(e.request);
+  //   })
+  // );
 
 });
 
 
 // Activation
-// self.addEventListener('activate', (e) => {
-//   e.waitUntil(
-//     caches.keys().then(keys => { 
-//       return Promise.all(keys
-//         .filter(key => key !== CACHE_NAME)
-//         .map(key => caches.delete(key))
-//       )
-//     })
-//   )
-// });
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => { 
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      )
+    })
+  )
+});
 
